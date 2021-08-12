b191807b72bf483dfb147fce73aed5612245e443
jmwallace77 <justin.wallace@revature.net>
2021-08-12 11:01:53 -0500

    added gate way. uri's are currently localhost

---
+++ P3GateWay/src/main/java/com/revature/P3GateWayApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3GateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3GateWayApplication.class, args);
+	}
+
+}
